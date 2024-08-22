5e1f48d64ff37fffde76e081e7b91036c367c16c
ukrnik <isaevv2003@gmail.com>
2024-08-22 03:14:09 +0200

    HW-1.0

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,25 @@
-function filterArray(numbers, value) {
-    let arrayResult = [];
-    for (const rightNumber of numbers) {
-        if (rightNumber > value) {
-            arrayResult.push(rightNumber);
-        }
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+
+    changeUsername(newName) {
+		profile.username = newName;
+    },
+
+    updatePlayTime(hours) {
+		profile.playTime += hours;
+    },
+
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
     }
-    return arrayResult;
-}
+};
 
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
